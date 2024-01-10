3a6fe3980b7d3769a5046ee0a121819e7f816e6f
alexkruchinin7 <137615394+alexkruchinin7@users.noreply.github.com>
2024-01-10 17:33:47 +0300

    1 commit

---
+++ src/task1/PrintHelloWorld.java
@@ -0,0 +1,13 @@
+package task1;
+
+public class PrintHelloWorld {
+
+        public static void main(String[] args) {
+
+            System.out.println("\"Hello world!\"");
+
+        }
+    }
+
+
+
